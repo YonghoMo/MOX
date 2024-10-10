8810af430d74aba9ce9cdec0ae0852766f0eb27b
ahdydgh123 <ahdydgh123@gmail.com>
2024-10-10 14:50:54 +0900

    캘린더 페이지 네비바, 파비콘 추가

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const io = socketIo(server, {
 // 정적 파일 제공 (HTML 파일 등)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/challenge.html');
-  });
+});
 
 // 방 목록을 관리할 객체
 let rooms = {};
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
 
     // 방 목록 요청 처리
     socket.on('get_rooms', () => {
-        socket.emit('room_list', rooms); 
+        socket.emit('room_list', rooms);
     });
 
     // 방 생성
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
         } else {
             rooms[roomName] = [];
             userRooms[socket.id] = roomName;  // 사용자가 만든 방 저장
-            updateRooms();  
+            updateRooms();
         }
     });
 
@@ -154,6 +154,6 @@ io.on('connection', (socket) => {
 });
 
 // 서버 실행
-server.listen(3000, () => {
-    console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
+server.listen(5000, () => {
+    console.log('서버가 http://localhost:5000 에서 실행 중입니다.');
 });
